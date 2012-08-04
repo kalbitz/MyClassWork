06d09ee65885ec0416060b6f081c635387072526
Katherine Albitz <kalbitz@san.rr.com>
2012-08-04 15:30:58 -0700

    Add songs

--- NuttinButCDs/NuttinButCDs/AlbumCollection.cs
+++ NuttinButCDs/NuttinButCDs/AlbumCollection.cs
@@ -26,18 +26,19 @@ namespace NuttinButCDs
             // TODO: Fill in from database
             
 
-            ObservableCollection<string> songs = new ObservableCollection<string>();
-            songs.Add("Song 1");
-            songs.Add("Song 2");
-            songs.Add("Song 3");
+            //ObservableCollection<string> songs = new ObservableCollection<string>();
+            //songs.Add("Song 1");
+            //songs.Add("Song 2");
+            //songs.Add("Song 3");
 
-            Uri image = new Uri("http://ecx.images-amazon.com/images/I/61QzCwpt7mL._SL75_.jpg");
+            //Uri image = new Uri("http://ecx.images-amazon.com/images/I/61QzCwpt7mL._SL75_.jpg");
 
-            Add(new Album("Goodbye Yellowbrick Road", "Elton John", "Rock", 1972, 4, "Best EJ there is!", null, null, songs));
-            Add(new Album("Fat Bottom Girls", "Queen", "Rock", 1974, 3, "", image, image, songs));
-            Add(new Album("The White Album", "Beatles", "Pop", 1969, 0, "", null, null, null));
-            Add(new Album("21", "Adele", "R&B", 2012, 2, "Not as good as they say", null, null, songs));
+            //Add(new Album("Goodbye Yellowbrick Road", "Elton John", "Rock", 1972, 4, "Best EJ there is!", null, null, songs));
+            //Add(new Album("Fat Bottom Girls", "Queen", "Rock", 1974, 3, "", image, image, songs));
+            //Add(new Album("The White Album", "Beatles", "Pop", 1969, 0, "", null, null, null));
+            //Add(new Album("21", "Adele", "R&B", 2012, 2, "Not as good as they say", null, null, songs));
 
+            // TODO: Presmuably there is a better way to fill the collection from the data in the db...
 
             NuttinButCDsDBDataSet CDsDataSet = new NuttinButCDsDBDataSet();
 
@@ -52,28 +53,33 @@ namespace NuttinButCDs
             DataTable albumDataTable = new DataTable("myAlbums");
             albumDataTable = albumsTableAdapter.GetData();
 
-            //DataTable songDataTable = new DataTable("mySongs");
-            //songDataTable = songsTableAdapter.GetData();
+            DataTable songDataTable = new DataTable("mySongs");
+            songDataTable = songsTableAdapter.GetData();
 
-            System.Data.DataRowCollection rows = albumDataTable.Rows;
-            foreach (System.Data.DataRow row in rows)
+            System.Data.DataRowCollection albumRows = albumDataTable.Rows;
+            foreach (System.Data.DataRow aRow in albumRows)
             {
+                ObservableCollection<string> songs = new ObservableCollection<string>();
+
+                string expression = String.Format("AlbumID = {0}", (int)aRow["AlbumID"]);
+                System.Data.DataRow[] songRows = songDataTable.Select(expression);
+
+                foreach (System.Data.DataRow sRow in songRows)
+                {
+                    songs.Add(ConvertFromDBVal<string>(sRow["SongName"]));
+                }
                 this.Add(new Album(
-                    ConvertFromDBVal<string>(row["AlbumName"]),
-                    ConvertFromDBVal<string>(row["ArtistName"]),
-                    ConvertFromDBVal<string>(row["Genre"]),
-                    (int)row["Year"],
-                    (int)row["Rating"],
-                    ConvertFromDBVal<string>(row["Comment"]),
-                    new Uri(ConvertFromDBVal<string>(row["AlbumImageSmall"])), 
-                    new Uri(ConvertFromDBVal<string>(row["AlbumImageLarge"])),
-                    null));
+                    ConvertFromDBVal<string>(aRow["AlbumName"]),
+                    ConvertFromDBVal<string>(aRow["ArtistName"]),
+                    ConvertFromDBVal<string>(aRow["Genre"]),
+                    (int)aRow["Year"],
+                    (int)aRow["Rating"],
+                    ConvertFromDBVal<string>(aRow["Comment"]),
+                    new Uri(ConvertFromDBVal<string>(aRow["AlbumImageSmall"])), 
+                    new Uri(ConvertFromDBVal<string>(aRow["AlbumImageLarge"])),
+                    songs));
 
             }
-
-            //var songs2 = new ObservableCollection<string>(
-            //    CDsDataSet.Tables["Songs"].AsEnumerable().Select(p => new String(p.Field<char[]>("SongName"))));
-
         }
 
         public new bool Remove(Album album)
