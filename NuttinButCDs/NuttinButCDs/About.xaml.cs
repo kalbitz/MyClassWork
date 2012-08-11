03e41aa72a2e4284451ecb0a80cad02ecefa6f61
Katherine Albitz <kalbitz@san.rr.com>
2012-08-11 09:52:09 -0700

    Tweak text.

--- NuttinButCDs/NuttinButCDs/About.xaml.cs
+++ NuttinButCDs/NuttinButCDs/About.xaml.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace NuttinButCDs
 {
-    /// <summary>
-    /// Interaction logic for About.xaml
-    /// </summary>
     public partial class About : Window
     {
         public About()
         {
             InitializeComponent();
             aboutTextBlock.Text =
-                "Brought to you by: Katherine R. Albitz\n\n" +
-                "Special Features:\n" +
-                "In Add CDs, the Artist name text box activates Find It on return.\n" +
-                "If there are no songs, you get a special message in the DataGrid RowDetailsTemplate box.\n" +
-                "The backend db is Microsoft SQL Server 2012 Express LocalDB.\n" +
-                "Hover over selected image cover in main window results in a larger image displayed.\n"
+                "Special Features:\n\n" +
+                "\u2022 In Add Albums and New Genre, the text box activates the action on return.\n" +
+                "\u2022 If there are no songs, you get a special message in the DataGrid RowDetailsTemplate box.\n" +
+                "\u2022 The backend db is Microsoft SQL Server 2012 Express LocalDB.\n" +
+                "\u2022 Hover over selected image cover in main window results in a larger image displayed.\n" +
+                "\u2022 Both Add Albums and New Genre feature an animated feedback text when you add an item." +
+                "\n\n" +
+                "Brought to you by: Katherine R. Albitz" 
                 ;
         }
 
